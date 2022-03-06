6b906e58984983ca371825648ab5b640e3a7eaca
Abhishek-S-B <abhisheksb9740@gmail.com>
2022-03-06 12:34:38 +0530

    sliding window and two pair

--- Java/slidingWindow/slidingWindow.java
+++ Java/slidingWindow/slidingWindow.java
@@ -1,5 +1,44 @@
 package Java.slidingWindow;
 
-public class slidingWindow {
+import java.util.Scanner;
 
+public class slidingWindow {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        slidingWindow sld = new slidingWindow();
+       // System.out.print(sld.maxSumBrtFrc(arr, n, k));
+        System.out.print(sld.maxSumOptimized(arr, n, k));
+    }
+    //Brute Force method TC = O(n*k)
+    public int maxSumBrtFrc(int[] arr, int n, int k) {
+        int max_sum = 0;
+        for(int i = 0; i < n - k +1; i++) {
+            int curr_sum = 0;
+            for(int j = 0; j < k; j++) {
+                curr_sum += arr[i+j];
+            }
+            max_sum = Math.max(curr_sum, max_sum);
+        }
+        return max_sum;
+    }
+
+    //Optimized solution TC = O(n)
+    public int maxSumOptimized(int[] arr, int n, int k) {
+        int max_sum = 0;
+        for(int i = 0; i < k; i++){
+            max_sum += arr[i];
+        }
+        int window_sum = max_sum;
+        for(int i = k; i < n; i++) {
+            window_sum += arr[i] - arr[i - k];
+            max_sum = Math.max(window_sum, max_sum);
+        }
+        return max_sum;
+    }
 }
---
+++ Java/twoPointers/sumPair.java
@@ -0,0 +1,29 @@
+package Java.twoPointers;
+
+import java.util.Scanner;
+
+public class sumPair {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int T = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print(sumOfPairNum(arr, n, T));
+    }
+    public static int sumOfPairNum(int[] arr, int n, int Target) {
+        int i = 0, j = n-1;
+        while(i < j) {
+            if((arr[i] + arr[j])==Target)
+                return 1;
+            else if((arr[i] + arr[j]) > Target)
+                j--;
+            else
+                i++;
+        }
+        return 0;
+    }
+
+}
