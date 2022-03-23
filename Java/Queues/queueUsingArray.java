4e25d5be73dda1b14ae88ad20435a497dc0ce2dc
Abhishek-S-B <abhisheksb9740@gmail.com>
2022-03-23 11:09:08 +0530

    queue implementation

---
+++ Java/Queues/queueUsingArray.java
@@ -0,0 +1,54 @@
+package Java.Queues;
+
+import java.util.*;
+
+public class queueUsingArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        queueUsingArray q = new queueUsingArray(n);
+
+        for(int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            q.enqueue(a,n);
+        }
+        System.out.print(q.element() + " ");
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+        System.out.print(q.element());
+    }
+    int rear;
+    int[] arr;
+    public queueUsingArray(int n) {
+         rear = -1;
+        arr = new int[n];
+    }
+
+    public void enqueue(int data, int n) {
+        if(rear == n-1) {
+            System.out.print("queue is full.");
+            return;
+        }
+        rear++;
+        arr[rear] = data;
+    }
+    public int dequeue() {
+        if(rear == -1) {
+            System.out.print("empty");
+            return -1;
+        }
+        int res = arr[0];
+
+        int i = 0;
+        while (i < rear) {
+            arr[i] = arr[i + 1];
+            i++;
+        }
+        rear--;
+        return res;
+    }
+    public int element() {
+        return arr[0];
+    }
+}
--- Java/Queues/queueusingLL.java
+++ Java/Queues/queueusingLL.java
@@ -1,2 +1,63 @@
-package Java.Queues;public class queueusingLL {
+package Java.Queues;
+
+import java.util.*;
+
+public class queueusingLL {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LL list = new LL();
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            list.enqueue(a);
+        }
+        list.print();
+        list.dequeue();
+        System.out.println();
+        list.print();
+
+    }
+    static class LL {
+       static Node front, rear;
+
+        static class Node {
+            Node next;
+            int data;
+
+            public Node(int data) {
+                this.data = data;
+                this.next = null;
+            }
+        }
+
+        public static void enqueue(int data) {
+            Node newNode = new Node(data);
+            if (front == null) {
+                front = newNode;
+                rear = newNode;
+                return;
+            }
+            rear.next = newNode;
+            rear = newNode;
+        }
+        public static void dequeue() {
+            if(front == null) {
+                return;
+            }
+            //System.out.print(front.data);
+            front = front.next;
+        }
+        public static void print() {
+            if(front == null) {
+                System.out.print("List is empty");
+                return;
+            }
+            Node curr = front;
+            while(curr != null) {
+                System.out.print(curr.data + " ");
+                curr = curr.next;
+            }
+
+        }
+    }
 }
